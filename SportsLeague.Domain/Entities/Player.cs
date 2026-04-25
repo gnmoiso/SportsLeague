76604f8f59e550779972397e87da7d8905afecaf
Moises De Jesus Gonzalez Navarro <moisoyt@gmail.com>
2026-04-25 15:46:22 -0500

    Fase 5 Completed witch test finished, all test are passing, no problems

--- SportsLeague.Domain/Entities/Player.cs
+++ SportsLeague.Domain/Entities/Player.cs
@@ -1,7 +1,4 @@
 ﻿using SportsLeague.Domain.Enums;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace SportsLeague.Domain.Entities;
 
@@ -18,5 +15,9 @@ public class Player : AuditBase
 
     // Navigation Property
     public Team Team { get; set; } = null!;
+
+    public ICollection<Goal> Goals { get; set; } = new List<Goal>();
+    public ICollection<Card> Cards { get; set; } = new List<Card>();
+
 }
 
